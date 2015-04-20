a1d38013aee408479dd61aacf39f0560f3c35edd
Peter Martin <pe7er@db8HQ>
2015-04-20 13:22:44 +0200

    first version with EN + NL language pack

--- db8tabreminder.php
+++ db8tabreminder.php
@@ -5,14 +5,30 @@
  * @copyright   Copyright (C) 2015 Peter Martin. All rights reserved.
  * @license     GNU General Public License version 2 or later.
  */
- 
+
 defined('_JEXEC') or die;
+
+jimport('joomla.plugin.plugin');
 
 class PlgSystemDb8tabreminder extends JPlugin
 {
-    public function onAfterRender()
-	{
-	
-	}
-		
+    protected $app;
+
+    public function onBeforeRender()
+    {
+        if ($this->app->isSite()) {
+            $body = $this->app->getBody();
+
+            $document = JFactory::getDocument();
+            $script = 'var title = document.title, newTitle = "'.$this->params->get('pretitle').'"';
+            if ($this->params->get('showtitle') == 1){
+                $script .= '+ title';
+            }
+            $script .= '+ "'.$this->params->get('posttitle').'";';
+            $script .= 'document.addEventListener("visibilitychange", function() {document.title = ((document.hidden) ? newTitle : title);});';
+
+            $document->addScriptDeclaration($script);
+        }
+    }
+
 }
