c923931e23dfd4bfcd5e3aa26cd57f98165ef7e4
Peter Martin <github@pe7er.com>
2015-04-20 10:24:19 +0200

    Create db8tabreminder.php

---
+++ db8tabreminder.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package     plg_system_db8tabreminder
+ * @author      Peter Martin, www.db8.nl
+ * @copyright   Copyright (C) 2015 Peter Martin. All rights reserved.
+ * @license     GNU General Public License version 2 or later.
+ */
+ 
+defined('_JEXEC') or die;
+
+class PlgSystemDb8tabreminder extends JPlugin
+{
+    public function onAfterRender()
+	{
+	
+	}
+		
+}
